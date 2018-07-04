8cef853f52db6a4ff981081a306220e1319989c1
Sain-R Edwards <ske@SKE-iMac.local>
2018-07-04 04:45:58 -0700

    Added podfiles, bridging header, firebase plist file.

---
+++ Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  myChat
+//
+//  Created by Sain-R Edwards on 7/4/18.
+//  Copyright © 2018 Swift Koding 4 Everyone. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <JSQMessagesViewController/JSQMessages.h>
+
+
+#endif /* Bridging_Header_h */
